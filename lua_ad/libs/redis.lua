e68be362984e85d5a4a9b4872b33cbcccc7881f2
coder-zhuyu <zhuyu1989.hi@gmail.com>
2016-07-23 23:39:57 +0800

    lua_ad

---
+++ lua_ad/libs/redis.lua
@@ -0,0 +1,25 @@
+local redis = require "resty.iredis"
+
+local red = redis:new({["ip"]="127.0.0.1", ["port"]=6379})
+
+local _M = {}
+
+function _M.get_from_redis(key)
+    local res, err = red:get(key)
+    if res then
+        return res
+    else
+        return nil
+    end
+end
+
+function _M.mget_from_redis(args)
+    local res, err = red:mget(unpack(args))
+    if res then
+        return res
+    else
+        return nil
+    end
+end
+
+return _M
